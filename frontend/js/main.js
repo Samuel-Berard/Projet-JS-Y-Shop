3ed6daf82b53980d1de2c63b9a196de322c9b5a7
clement belmondo <clement.belmondo@outlook.fr>
2026-04-30 10:23:40 +0200

    Mise à jour du frontend et correction du main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,9 @@
-const header = document.querySelector('.header')
+const header = document.querySelector('header')
 
-window.addEventListener("scroll", function () {
-    header.classList.toggle("sticky", this.window.scrollY > 0);
-})
+if (header) {
+    window.addEventListener("scroll", function () {
+        header.classList.toggle("sticky", window.scrollY > 0);
+    })
+}
 
 let menu = document.querySelector('#menu-icon');
