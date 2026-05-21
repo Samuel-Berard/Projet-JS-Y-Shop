ef87e7e110ac29a5e01085d729a9283d451e4217
samuel.berard%20ynov.com <samuel.berard%20ynov.com>
2026-05-21 20:45:13 +0200

    Fix back + autres

--- backend/router/panier.js
+++ backend/router/panier.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controller/panier');
 
-//recuperer tous les paniers
-router.get('/', cartController.recupererTousPaniers);
-
 //passer commande
 router.post('/commander', cartController.passerCommande);
 
