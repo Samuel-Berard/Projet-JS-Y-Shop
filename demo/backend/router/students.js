73da6b9f932166f0985eeeda2f36f6773d686050
clement belmondo <clement.belmondo@outlook.fr>
2026-05-21 19:01:22 +0200

    feat/tkt

--- demo/backend/router/students.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controller/students')
-
-router.get('/students/', controller.getAllStudents)
-router.get('/students/:id', controller.getStudentById)
-
-module.exports = router
